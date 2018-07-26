688f1efe51d9fbba7e107689e29a8e885775db90
Matthew Fontana <mcf@spotify.com>
2018-07-25 20:27:23 -0700

    Massive work to get the form actually working and submitting data!  Still problems with closing the dialog and starting a new round

--- src/main/js/newRoundCard.js
+++ src/main/js/newRoundCard.js
@@ -6,7 +6,6 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -14,21 +13,6 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
 
 const cardStyle = theme => ({
   root: {
@@ -79,15 +63,12 @@ class NewRoundCardUnstyled extends React.Component {
   }
 
   handleSubmit(event) {
+    console.log('Received Submit');
     console.log(event.target);
     this.handleClose();
   }
 
   render() {
-    const players = this.props.combatants.filter(function(combatant){
-      return combatant.npc != true;
-    });
-
     return (
       <div className={this.props.classes.root}>
         <Card className={this.props.classes.card}>
@@ -98,46 +79,88 @@ class NewRoundCardUnstyled extends React.Component {
             <Typography className={this.props.classes.pos} color="textSecondary">
               This wizard will help you collect all the player initatives and re-roll all npc initatives automatically
             </Typography>
+            <NewRoundForm combatants={this.props.combatants} open={this.state.open} close={this.handleClose}/>
           </CardContent>
           <CardActions>
             <Button size="small" onClick={this.handleOpen}>New Round</Button>
           </CardActions>
         </Card>
-        <Dialog
-          open={this.state.open}
-          onClose={this.handleClose}
-          aria-labelledby="form-dialog-title"
-        >
-          <DialogTitle id="form-dialog-title">Enter new player initiative rolls</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Please have the players roll new initiatives and enter them below.  On Submitting npcs will automatically re-roll initiative.
-            </DialogContentText>
-            {
-              players.map( player => {
-                return <TextField
-                  key={player.id}
-                  autoFocus
-                  margin="dense"
-                  id="name"
-                  label={player.name}
-                  type="email"
-                  fullWidth
-                />
-              })
-            }
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={this.handleClose} color="primary">
-              Submit
-            </Button>
-          </DialogActions>
-        </Dialog>
       </div>
     )
   }
 }
 export const NewRoundCard = withStyles(cardStyle)(NewRoundCardUnstyled);
+
+
+class NewRoundForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inititatives: new Map(),
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    let inititatives = this.state.inititatives;
+    inititatives.set(event.target.id, event.target.value);
+    this.setState({inititatives: inititatives});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.inititatives);
+
+    //TODO figure out why this doesn't close the Dialog
+    this.props.close;
+  }
+
+  render() {
+    const players = this.props.combatants.filter(function(combatant){
+      return combatant.npc != true;
+    });
+
+    return (
+      <Dialog
+        open={this.props.open}
+        onClose={this.props.close}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Enter new player initiative rolls</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please have the players roll new initiatives and enter them below.  On Submitting npcs will automatically re-roll initiative.
+          </DialogContentText>
+          <form id="newRoundForm" onSubmit={this.handleSubmit}>
+            {
+              players.map( player => {
+                const stringId = player.id.toString();
+                return <TextField
+                  key={player.id}
+                  autoFocus
+                  margin="dense"
+                  id={stringId}
+                  label={player.name}
+                  type="number"
+                  fullWidth
+                  onChange={this.handleChange}
+                />
+              })
+            }
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.props.close} color="primary" form="newRoundForm" type="reset">
+            Cancel
+          </Button>
+          <Button color="primary" form="newRoundForm" type="submit">
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
