d731bd4d32a3ffc8500b88e3ce1fb6aefd593f07
Matthew Fontana <mcf@spotify.com>
2018-07-29 09:54:05 -0400

    formatting

--- src/main/js/newRoundCard.js
+++ src/main/js/newRoundCard.js
@@ -13,7 +13,6 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-
 const cardStyle = theme => ({
   root: {
     padding: theme.spacing.unit * 2,
