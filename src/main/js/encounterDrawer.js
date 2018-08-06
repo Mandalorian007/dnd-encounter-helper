10ca975a180b442f9ac86444607a12e128e0d21f
Matthew Fontana <mcf@spotify.com>
2018-08-06 11:07:33 -0400

    Import cleanup

--- src/main/js/encounterDrawer.js
+++ src/main/js/encounterDrawer.js
@@ -10,7 +10,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { CombatantList } from "./combatantList";
-import NewRoundForm from "./newRoundForm";
 import NewFixedStatCombatantForm from "./newFixedStatCombatantForm";
 import NewNpcsFromTemplateForm from "./newNpcsFromTemplateForm";
 
