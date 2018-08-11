3677d247f0f3880dd455410d7013b1d83c35cd02
Matthew Fontana <mcf@spotify.com>
2018-08-11 18:02:19 -0400

    Fixed the sliders and "fixed" the combatant info

--- src/main/java/com/dnd/tools/encounterhelper/loader/TestDataCombatantLoader.java
+++ src/main/java/com/dnd/tools/encounterhelper/loader/TestDataCombatantLoader.java
@@ -16,39 +16,61 @@ public class TestDataCombatantLoader implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     Combatant combatant = new Combatant();
-    combatant.setName("Woopa Gator");
-    combatant.setArmourClass(28);
-    combatant.setMaxHp(1);
-    combatant.setInitativeBonus(11);
-    combatant.setPassivePerception(6);
-    combatant.setCurrentHp(1);
-    combatant.setCurrentInitiative(21);
-    combatant.setComment("I am too fast for you!");
-    combatant.setNpc(true);
+    combatant.setName("Cora Valentine");
+    combatant.setArmourClass(18);
+    combatant.setMaxHp(90);
+    combatant.setInitativeBonus(5);
+    combatant.setPassivePerception(18);
+    combatant.setCurrentHp(81);
+    combatant.setCurrentInitiative(25);
+    combatant.setComment("You'll never see me coming!");
+    combatant.setNpc(false);
 
     Combatant combatant2 = new Combatant();
-    combatant2.setName("BSF");
-    combatant2.setArmourClass(18);
-    combatant2.setMaxHp(100);
-    combatant2.setInitativeBonus(5);
-    combatant2.setPassivePerception(13);
-    combatant2.setCurrentHp(1);
-    combatant2.setCurrentInitiative(8);
-    combatant2.setComment("Wackamole King");
+    combatant2.setName("Lambert LaMure");
+    combatant2.setArmourClass(20);
+    combatant2.setMaxHp(115);
+    combatant2.setInitativeBonus(6);
+    combatant2.setPassivePerception(14);
+    combatant2.setCurrentHp(115);
+    combatant2.setCurrentInitiative(22);
+    combatant2.setComment("I'll cut off your head!");
     combatant2.setNpc(false);
 
     Combatant combatant3 = new Combatant();
-    combatant3.setName("The Dude");
-    combatant3.setArmourClass(18);
-    combatant3.setMaxHp(100);
-    combatant3.setInitativeBonus(2);
-    combatant3.setPassivePerception(13);
-    combatant3.setCurrentHp(1);
-    combatant3.setCurrentInitiative(4);
-    combatant3.setComment("Win");
-    combatant3.setNpc(false);
+    combatant3.setName("Llervu Andavel");
+    combatant3.setArmourClass(17);
+    combatant3.setMaxHp(80);
+    combatant3.setInitativeBonus(3);
+    combatant3.setPassivePerception(16);
+    combatant3.setCurrentHp(80);
+    combatant3.setCurrentInitiative(14);
+    combatant3.setComment("Vandila guide my might!");
+    combatant3.setNpc(true);
 
-    combatantRepository.saveAll(Arrays.asList(combatant, combatant2, combatant3));
+    Combatant combatant4 = new Combatant();
+    combatant4.setName("Israel Ben Esar");
+    combatant4.setArmourClass(17);
+    combatant4.setMaxHp(78);
+    combatant4.setInitativeBonus(5);
+    combatant4.setPassivePerception(19);
+    combatant4.setCurrentHp(78);
+    combatant4.setCurrentInitiative(19);
+    combatant4.setComment("I've no time for words! ONLY ARROWS!");
+    combatant4.setNpc(false);
+
+    Combatant combatant5 = new Combatant();
+    combatant5.setName("Ristoro de Caldrini");
+    combatant5.setArmourClass(18);
+    combatant5.setMaxHp(96);
+    combatant5.setInitativeBonus(3);
+    combatant5.setPassivePerception(10);
+    combatant5.setCurrentHp(96);
+    combatant5.setCurrentInitiative(17);
+    combatant5.setComment("The tide of battle is mine to command!");
+    combatant5.setNpc(true);
+
+    combatantRepository.saveAll(Arrays.asList(combatant, combatant2, combatant3, combatant4, combatant5));
 
   }
 }
