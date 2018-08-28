1b3ec0ca5dc571bc944f8ec1aad2d34346daeaad
Matthew Fontana <mandalorian007@gmail.com>
2018-08-28 09:12:48 -0400

    convert App to a functional component

--- src/main/js/app.tsx
+++ src/main/js/app.tsx
@@ -4,13 +4,7 @@ import * as ReactDOM from "react-dom";
 import EncounterDrawer from "./encounterDrawer";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-class App extends React.Component<any, any> {
-  render() {
-    return (
-      <EncounterDrawer/>
-    )
-  }
-}
+const App = () => <EncounterDrawer/>;
 
 ReactDOM.render(
   <MuiThemeProvider>
