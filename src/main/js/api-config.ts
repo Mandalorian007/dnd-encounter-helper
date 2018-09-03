8bc71861c52692abc80d8fb3cd2a01cb0888ed27
Matthew Fontana <mandalorian007@gmail.com>
2018-09-03 11:40:55 -0400

    docker work to get it running in GCP

--- src/main/js/api-config.ts
+++ src/main/js/api-config.ts
@@ -2,10 +2,9 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if(hostname === '35.224.244.25') {
-    backendHost = 'http://35.224.244.25';
+if(hostname !== 'localhost') {
+    backendHost = `http://${hostname}`;
 } else {
     backendHost = 'http://localhost:8080';
 }
-
 export const API_ROOT = `${backendHost}`;
