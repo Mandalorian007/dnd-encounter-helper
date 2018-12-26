4ed1f05dca55f88b59b19a2ee1fb9640d6251384
Matthew Fontana <mcf@spotify.com>
2018-12-26 15:33:14 -0500

    Adding spellcasting to the API

--- src/main/java/com/dnd/tools/encounterhelper/monster/model/InnateSpellCasting.java
+++ src/main/java/com/dnd/tools/encounterhelper/monster/model/InnateSpellCasting.java
@@ -1,5 +1,44 @@
 package com.dnd.tools.encounterhelper.monster.model;
 
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embeddable;
+import lombok.Data;
+
+@Data
+@Embeddable
 public class InnateSpellCasting {
+
+  @ElementCollection
+  @Column(length = 10000)
+  private List<String> headerEntries;
+
+  private String spellStat;
+
+  @ElementCollection
+  private List<String> atWill;
+
+  @ElementCollection
+  private List<String> one;
+
+  @ElementCollection
+  private List<String> oneEach;
+
+  @ElementCollection
+  private List<String> two;
+
+  @ElementCollection
+  private List<String> twoEach;
+
+  @ElementCollection
+  private List<String> three;
+
+  @ElementCollection
+  private List<String> threeEach;
+
+  @ElementCollection
+  @Column(length = 10000)
+  private List<String> footerEntries;
 
 }
