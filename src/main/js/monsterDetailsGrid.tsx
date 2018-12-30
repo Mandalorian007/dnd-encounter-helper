dc62e0a6c82c5f6dec391157a01e962faf3222a6
TKDMaster12 <bierstephen@gmail.com>
2018-12-30 12:53:12 -0500

    quickfix

--- src/main/js/monsterDetailsGrid.tsx
+++ src/main/js/monsterDetailsGrid.tsx
@@ -100,6 +100,27 @@ class MonsterDetailsGrid extends React.Component<any, State> {
             return "+" + value;
     };
 
+    compareValues = (key) => {
+        return function(a, b) {
+            if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+                // property doesn't exist on either object
+                return 0;
+            }
+
+            const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+            const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+            let comparison = 0;
+
+            if (varA > varB) {
+                comparison = 1;
+            } else if (varA < varB) {
+                comparison = -1;
+            }
+
+            return (comparison);
+        };
+    }
+
     getActions = (actionList, subHeaderName) => {
         const rowId = "row-data-" + subHeaderName;
         if (Array.isArray(actionList) && actionList.length) {
